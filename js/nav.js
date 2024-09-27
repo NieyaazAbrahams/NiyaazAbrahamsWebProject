0d703c9da1f3906baca2092dcc8af51a09d98ca8
NieyaazAbrahams <bsub26557@gmail.com>
2024-09-28 01:57:50 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM fully loaded");
 });
 
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM fully loaded");
 
